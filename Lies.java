d97173d87ccc68a5fccbc14a53d2aa87c4163dd9
Abhishek Sardar <143304955+Abhishek-sard@users.noreply.github.com>
2024-08-02 23:50:30 +0545

    Add files via upload

---
+++ Lies.java
@@ -0,0 +1,15 @@
+public class Lies {
+    public static void main(String[] args) {
+        int x = 21193;
+        if(x>21001){
+            System.out.println("yes in this number");
+        }
+        else if(x<21200){
+            System.out.println("yes it is less number");
+        }
+        else{
+            System.out.println("insdie it");
+        }
+
+    }
+}
